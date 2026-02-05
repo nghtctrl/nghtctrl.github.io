f649037bc78c4d146fe3aaa625b9b91bcfcc31db
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2026-02-04 22:21:23 -0600

    Add astro-icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), icon()],
 
   vite: {
     plugins: [tailwindcss()],
