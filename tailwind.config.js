db884f3bdc2a6250ae321643696a01554772ca17
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2026-02-14 21:46:35 -0600

    Adjust tailwind typographic settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,23 @@ import typography from '@tailwindcss/typography';
 /** @type {import('tailwindcss').Config} */
 export default {
   plugins: [typography],
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            h1: { marginTop: '1em', marginBottom: '0.5em' },
+            h2: { marginTop: '1em', marginBottom: '0.5em' },
+            h3: { marginTop: '1em', marginBottom: '0.5em' },
+            h4: { marginTop: '1em', marginBottom: '0.5em' },
+            h5: { marginTop: '1em', marginBottom: '0.5em' },
+            h6: { marginTop: '1em', marginBottom: '0.5em' },
+            p: { marginTop: '0.5em', marginBottom: '0.5em' },
+            li: { marginTop: '0.5em', marginBottom: '0.5em' },
+            img: { marginTop: 0, marginBottom: 0 },
+          },
+        },
+      }),
+    },
+  },
 };
