f8b077624d41a013d565c30e7e60ce767d7b3ba3
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2026-02-16 18:04:19 -0600

    Add dark/light mode toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,13 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  plugins: [typography],
+  darkMode: 'class',
   theme: {
     extend: {
+      colors: {
+        'dark-bg': '#151515',
+        'lighter-dark-bg': '#202022',
+      },
       typography: (theme) => ({
         DEFAULT: {
           css: {
@@ -17,6 +21,20 @@ export default {
             p: { marginTop: '0.5em', marginBottom: '0.5em' },
             li: { marginTop: '0.5em', marginBottom: '0.5em' },
             img: { marginTop: 0, marginBottom: 0 },
+          },
+        },
+        dark: {
+          css: {
+            color: theme('colors.gray.300'),
+            a: { color: theme('colors.blue.400') },
+            h1: { color: theme('colors.gray.100') },
+            h2: { color: theme('colors.gray.100') },
+            strong: { color: theme('colors.gray.100') },
+            code: { color: theme('colors.gray.100') },
+            blockquote: {
+              color: theme('colors.gray.200'),
+              borderLeftColor: theme('colors.gray.700'),
+            },
           },
         },
       }),
