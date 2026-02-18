e4f8222c10a5e13e6514246d62e4b71b333b1b92
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2026-02-17 21:47:28 -0600

    Switch to GitHub Pages for hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nghtctrl.github.io',
   integrations: [react(), mdx(), icon()],
-
   vite: {
     plugins: [tailwindcss()],
   },
