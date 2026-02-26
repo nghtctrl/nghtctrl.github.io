eb09a54219c3ec1a77daff44d0fa99087c0525ef
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2026-02-26 11:28:44 -0600

    Use Astro SEO for SEO stuff

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const SITE_CONFIG = {
+  title: 'Jiho Kim',
+  description:
+    'I am a human-computer interaction researcher studying how AI impacts the way we think, learn, express ourselves, and communicate with others.',
+  siteUrl: 'https://jihokim.dev/',
+  ogImage: '/src/assets/og-image.png',
+};
