01baf10626a2e82ddd6d6a940c695dbd3cc72b88
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2026-03-31 10:22:59 -0500

    Update rest of the domain links

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const SITE_CONFIG = {
   title: 'Jiho Kim',
   description:
     'I am a human-computer interaction researcher studying how AI impacts the way we think, learn, express ourselves, and communicate with others.',
-  siteUrl: 'https://jihokim.dev/',
+  siteUrl: 'https://jihokim.com/',
   ogImage:
     'https://raw.githubusercontent.com/nghtctrl/nghtctrl.github.io/refs/heads/main/src/assets/og-image.png',
 };
